70e193d5973f31623cf4721f70982c74cfa25c31
jferrarot <jferrarot@gmail.com>
2022-08-23 11:48:19 -0500

    aa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+//functional (no constructor)
+
+//pure function always return the same value
+//impure functions returns different values
+  //also if it creates a side effect outside its bounds (c changing)
+
+import {useState,useEffect} from "react";
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
+const App = () =>{
+
+  const [searchField,setSearchField] = useState(''); // [value,setValue] it calls render when searchField value changes
+  const [monsters,setMonsters]=useState([]);
+  const [filteredMonsters,setFilterMonsters]=useState(monsters);
+
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=>response.json())
+    .then((users)=>setMonsters(users));
+  },[]);
+
+  useEffect(()=>{
+    const newFilteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilterMonsters(newFilteredMonsters);
+  },[monsters,searchField]) //it means, only render when monsters or searchField changes
+
+
+  const onSearchChange = (event) =>{
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1 className='app-title'>Monster Rolodex</h1>
+
+      <SearchBox
+        searchClassName = {'monsters-search-box'}
+        onChangeHandler ={onSearchChange}
+        searchPlaceholder ={'Search monsters'}
+      />
+
+      <CardList  
+        monsters={filteredMonsters}
+      />
+  </div>
+  )
+
+
 }
 
 export default App;
